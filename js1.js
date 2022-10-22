ecb57eef304fb473ce58798bc4d7d9b1892465c8
Ignat1995 <95sprite14@mail.ru>
2022-10-22 22:18:43 +0300

    lesson 1 VS

---
+++ js1.js
@@ -0,0 +1,63 @@
+// ПЕРЕМЕННЫЕ
+
+
+// let yolochka4 = 4;
+// let yolochka6 = 6;
+// let yolochka_name = " Zelyonaya"
+
+
+// let result = yolochka4 + yolochka6;
+// let result_2 = yolochka6 + yolochka_name;
+
+// console.log(yolochka4+yolochka6)
+
+// console.log(result);
+// console.log(result_2);
+
+
+
+// БУЛЕВЫЙ ТИП ДАННЫХ
+
+
+
+// let t = true
+// let f = false
+
+// console.log(t + f)
+
+// if (f) {
+// let apples = 5+3
+// console.log("If true = " + apples)
+// }
+// else if (f){
+//     let milk = 2 
+//     console.log("else if = " + milk)
+// }
+
+//  else {
+// let meat = 2 + 2
+// console.log("If not true = " + meat)
+
+    
+// }
+
+let age = "18"
+
+if (age >= 18 && age < 60) {
+
+    console.log("Full content")
+}
+
+else if (age < 18) {
+
+    console.log("Multiki")
+}
+
+else if (age >= 60) {
+
+    console.log("Kultura")
+}
+
+else {
+    console.log("Good luck!")
+}
